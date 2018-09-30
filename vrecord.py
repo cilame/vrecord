83ef833ff5ae549a28d6b16d4c69e13b87d01e96
cilame <2473495041@qq.com>
2018-09-30 21:08:46 +0800

    add

--- vrecord.py
+++ vrecord.py
@@ -3,62 +3,58 @@ import pythoncom
 import win32api
 import tkinter
 
+
+
+
+
+
+def create_window():
+    root = tkinter.Tk()
+    vvvv = tkinter.IntVar(value=1)
+    ffff = tkinter.Checkbutton(root,text='是否使用模板顺序（选择后，跳过未翻译将无效）',variable=vvvv)
+    ffff.pack()
+    return root
+
+
 class key_mouse_manager():
 
     def __init__(self):
-        self.root = tkinter.Tk()
-        self.root.withdraw() # 需要一个被隐藏的主界面，一切额外窗口循环需要该窗口存在才能
+        self.root = create_window()
         self.edit_window = None
         self.edit_toggle = True
     
     def on_key_down(self,event):
-
         if event.Key in ['F1','F2','F3','F4']:
             if event.Key == 'F1': self.func_F1()
             if event.Key == 'F2': self.func_F2()
             if event.Key == 'F3': self.func_F3()
             if event.Key == 'F4': self.func_F4()
             return False
-
-        if event.Key in ['Lwin',
+        elif event.Key in ['Lwin',
                          'Rwin',
                          'Lcontrol',
                          'Rcontrol',
                          'Lmenu',
-                         'Rmenu']:
-            # 目前测试似乎存在组合键异常的问题，暂时去除问题
+                         'Rmenu']: # 目前测试似乎存在组合键异常的问题，暂时去除
             return False
-
-        if event.Key == 'Escape':
-            # Esc 退出程序
+        elif event.Key == 'Escape':
             hookmanager.UnhookKeyboard()
             hookmanager.UnhookMouse()
             win32api.PostQuitMessage()
             self.root.destroy()
             return False
-
-
-        if self.keyIsPressed:
+        else:
             return True
-        self.keyIsPressed = True
-        return True
-
-    def on_key_up(self,event):
-        self.keyIsPressed = False
-        return False
 
 
     def func_F1(self):
         print('F1')
-        # 这里主要处理的创建脚本管理的窗口
         if self.edit_window is None:
-            self.edit_window = tkinter.Toplevel()
-            # 该窗口用于配置录制的脚本方式已经配置执行方式
-            # 各种功能配置功能需要在这里实现
+            self.edit_window = self.root
             self.edit_window.mainloop()
         elif self.edit_window is not None:
             if self.edit_toggle == True:
-                self.edit_window.withdraw() # 这里用隐藏窗口会更加省资源一点。
+                self.edit_window.withdraw()
                 self.edit_toggle = False
             else:
                 self.edit_window.deiconify() # 显示隐藏窗口。withdraw 和 deiconify 为配对的函数。
@@ -85,8 +81,8 @@ class key_mouse_manager():
         msg = event.MessageName
         tim = event.Time
         pos = event.Position
-        print("MessageName:",   msg)
-        print("Time:",          tim)
+        #print("MessageName:",   msg)
+        #print("Time:",          tim)
         print("Position:",      pos)
         return True
 
@@ -95,7 +91,6 @@ if __name__ == '__main__':
     mykbmanager = key_mouse_manager()
     hookmanager = pyHook.HookManager()
     hookmanager.KeyDown  = mykbmanager.on_key_down
-    hookmanager.KeyUp    = mykbmanager.on_key_up
     hookmanager.MouseAll = mykbmanager.on_mouse_event
     hookmanager.HookKeyboard()
     hookmanager.HookMouse()
